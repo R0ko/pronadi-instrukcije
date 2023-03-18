3cb36d85b0932f2cc39cd806aacfc3c9c2357976
Roko <roko.magdalenic7@gmail.com>
2023-03-18 12:23:20 +0100

    [WIP] add/delete/update instructor data contact-like

--- backend/routes/instructorData.js
+++ backend/routes/instructorData.js
@@ -24,22 +24,111 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/instructorData
 // @desc    Add new instructor data
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add new instructor data');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      name,
+      email,
+      phone,
+      subject,
+      price,
+      distance,
+      numberOfInstructionsHeld,
+      date,
+    } = req.body;
+
+    try {
+      const newInstructorCard = new InstructorCard({
+        name,
+        email,
+        phone,
+        subject,
+        price,
+        distance,
+        numberOfInstructionsHeld,
+        date,
+        user: req.user.id,
+      });
+      const instructorCard = await newInstructorCard.save();
+      res.json(instructorCard);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/instructorData/:id
 // @desc    Update instructor data
 // @access  Private
-router.put('/:id', (req, res) => {
-  res.send('Update instructor data');
+router.put('/:id', auth, async (req, res) => {
+  const {
+    name,
+    email,
+    phone,
+    subject,
+    price,
+    distance,
+    numberOfInstructionsHeld,
+    date,
+  } = req.body;
+
+  // Build instructor data
+  const instructorDataFields = {};
+  if (name) instructorDataFields.name = name;
+  if (email) instructorDataFields.email = email;
+  if (phone) instructorDataFields.phone = phone;
+  if (subject) instructorDataFields.subject = subject;
+  if (price) instructorDataFields.price = price;
+  if (distance) instructorDataFields.distance = distance;
+  if (numberOfInstructionsHeld)
+    instructorDataFields.numberOfInstructionsHeld = numberOfInstructionsHeld;
+  if (date) instructorDataFields.date = date;
+
+  try {
+    let instructor = await InstructorCard.findById(req.params.id);
+    if (!instructor)
+      return res.status(404).json({ msg: 'Instructor not found' });
+    // Make sure user owns instructor (nepotrebno)
+    if (instructor.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized!' });
+    }
+    instructor = await InstructorCard.findByIdAndUpdate(
+      req.params.id,
+      { $set: instructorDataFields },
+      { new: true }
+    );
+    res.json(instructor);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   DELETE api/instructorData/:id
 // @desc    Delete instructor data
 // @access  Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete instructor data');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let instructor = await InstructorCard.findById(req.params.id);
+    if (!instructor)
+      return res.status(404).json({ msg: 'Instructor not found' });
+    // Make sure user owns instructor (nepotrebno)
+    if (instructor.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized!' });
+    }
+    await InstructorCard.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'Contact removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
